637efcf340bad43fd1153a5f24a79f3bc6e81549
Kalana Helanjith <kalanahelanjith@gmail.com>
2025-01-20 12:50:12 +0530

    init👌

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+
+import './App.css'
+import ProductCard from './components/productCard'
+
+
+function App() {
+  
+
+  return (
+    <>
+    <ProductCard name="Audio Setup " price="4500/-" 
+    
+    description="hsdkjdskkd bhdbhbh bfhbdb"
+
+    photoUrl="https://img.drz.lazcdn.com/g/kf/S6cfb0ee8d9f74ebb9660fd04b3622e364.jpg_720x720q80.jpg_.webp"/>
+    
+
+    <ProductCard name="Jedel RGB Gaming Headset"
+
+    price="3450/-"
+
+    description="Jedel RGB Gaming Headset High Bass Quality sound Headset with 6 months warranty"
+
+    photoUrl="https://img.drz.lazcdn.com/static/lk/p/0113d6106130831c9b6b54a6a942fed9.png_720x720q80.png_.webp"/>
+    
+    
+     
+    </>
+  )
+}
+
+export default App
