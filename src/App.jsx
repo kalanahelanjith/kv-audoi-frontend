f44c801b2cad7396d385f15d16fdf11a9ed1182a
Kalana Helanjith <kalanahelanjith@gmail.com>
2025-02-16 00:30:56 +0530

    day 12 ✔

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import './App.css'
 import AdminPage from './pages/admin/adminPage';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/Home/homePage';
+import Testing from './components/testing';
+import LoginPage from './pages/login/login';
+import { Toaster } from 'react-hot-toast';
 
 
 
@@ -12,7 +15,11 @@ function App() {
 
   return (
     <BrowserRouter>
+    <Toaster position='top-right'/>
     <Routes path="/*">
+
+    <Route path="/testing" element={<Testing/>}/>
+    <Route path="/login" element={<LoginPage/>}/>
 
     <Route path="/admin/*" element={<AdminPage/>}/>
     <Route path="/*" element={<HomePage/>}/>
